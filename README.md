245b7b31549fda66b84b7dfed379d2e52a3a20ec
Mrudula <mvarade8@gmail.com>
2015-05-18 07:23:14 +0530

    Working code with unique filters, editable topic names,draggable functionalities

--- README.md
+++
@@ -1,1 +0,0 @@
-# Learning-Course
--- js/maincontroller.js
+++ js/maincontroller.js
@@ -15,7 +15,7 @@ app.controller('LearnCourse', function ($scope) {
 			{   
 				id : 2, 
 				image: "course_image.jpg",
-				title : "Advanced Lorem Ipsum", 
+				title : "Advanced LoremIpsum", 
 				subject : "Math", 
 				grade : 2, 
 				noOfVideos : 1,
@@ -59,124 +59,42 @@ app.controller('LearnCourse', function ($scope) {
 					[   
 						{
 		   					id: 01,
-							name:"Topic Name1",
+							name:"Topic 1",
 							lessons:
 		  						[
-				   					{
-										id : 1, 
-										image: "course_image.jpg",
-										title : "Lorem Ipsum", 
-										subject : "Math", 
-										grade : 2, 
-										noOfVideos : 1, 
-										noOfDocuments : 1,
-										noOfQuestions : 2
-									},
-									{   
-										id : 2, 
-										image: "course_image.jpg",
-										title : "Advanced Lorem Ipsum", 
-										subject : "Math", 
-										grade : 2, 
-										noOfVideos : 1,
-										noOfDocuments : 0, 
-										noOfQuestions : 1
-									},
-									{ 
-										id : 3, 
-										image: "course_image.jpg",
-										title : "Lorem Ipsum", 
-										subject : "English", 
-										grade : 1, 
-										noOfVideos : 0, 
-										noOfDocuments : 1,
-										noOfQuestions : 1
-									},
-									{ 
-										id : 4, 
-										image: "course_image.jpg",
-										title : "Lorem Ipsum", 
-										subject : "Science", 
-										grade : 1, 
-										noOfVideos : 1, 
-										noOfDocuments : 1,
-										noOfQuestions : 2
-									},
-									{ 
-										id : 5, 
-										image: "course_image.jpg",
-										title : "Lorem Ipsum", 
-										subject : "Computers", 
-										grade : 2, 
-										noOfVideos : 1, 
-										noOfDocuments :1, 
-										noOfQuestions : 1
-									}
+				   					
 								],
 
 		   					proficiency:"easy"
 		  				},
 						{
 							id: 02,
-							name:"Topic Name2",
+							name:"Topic 2",
 							lessons:
 							  	[
-							 	 	{
-										id : 1, 
-										image: "course_image.jpg",
-										title : "Lorem Ipsum", 
-										subject : "Math", 
-										grade : 1, 
-										noOfVideos : 1, 
-										noOfDocuments : 1,
-										noOfQuestions : 2
-												
-									}
-
 							  	],
 
-							proficiency:"hard"
+							proficiency:"easy"
 						},
 						{
 		                    id: 01,
-						    name:"Topic Name3",
+						    name:"Topic 3",
 						    lessons:
 		  						[
-									{
-										id : 1, 
-										image: "course_image.jpg",
-										title : "Lorem Ipsum", 
-										subject : "Math",
-										grade : 1, 
-										noOfVideos : 1, 
-										noOfDocuments : 1,
-										noOfQuestions : 2 
 									
-									},{
-										id : 2, 
-										image: "course_image.jpg",
-										title : "Lorem Ipsum", 
-										subject : "Math",
-										grade : 1, 
-										noOfVideos : 1, 
-										noOfDocuments : 1,
-										noOfQuestions : 2 
-									
-									}
 		  						],
 
-		   					proficiency:"easy"
+		   					proficiency:"medium"
 		 				}
 	 
 	 
 					]
 			};
-
+ 	$scope.proficiency=["easy","medium","hard"];
 		
 	$scope.lessons=$scope.allLessons;
 
 
-	
 	$scope.update=function() {
 		var lessonList=[];
 		var index;
@@ -209,13 +127,30 @@ app.controller('LearnCourse', function ($scope) {
  //   $scope.user = {
 	// name: 'awesome user'
 	// 			}; 
-   $scope.onDragComplete=function(data,evt){
+    $scope.onDragSuccess=function(data,evt){
 	   console.log("drag success, data:", data);
+
 	}
-	$scope.onDropComplete=function(data,evt){
+	$scope.onDropComplete=function(data,evt,lessons){
 		console.log("drop success, data:", data);
+		console.log(lessons)
+		index = lessons.indexOf(data);
+		if ( index == -1){
+			lessons.push(data);
+		}else{
+			alert("Lesson already exist , can not be added again to same topic");
+		}
 	} 
 
+	$scope.deleteWish=function(lesson,lessons){
+		console.log("Deleting",lesson);
+		
+		var index=lessons.indexOf(lesson);
+		if( index > -1){
+
+			lessons.splice(index,1);
+		}
+	}
 
 
 });
